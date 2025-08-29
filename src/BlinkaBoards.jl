a773cc40ec851ba31ea5f4be939e3c35133fab21
Nick Stone <nicholasestone@proton.me>
2025-08-29 11:23:21 -0500

    Added functionality for digital IO

--- src/BlinkaBoards.jl
+++ src/BlinkaBoards.jl
@@ -2,7 +2,8 @@ module BlinkaBoards
 
 using PyCall
 
-export BlinkaBoard, i2c
+export BlinkaBoard, i2c, DigitalIOPin, setinput!, setoutput!, rawiopin, isinput, isoutput
+export digitalread, digitalwrite!
 
 """
 ```julia
@@ -17,8 +18,9 @@ commands should be executed in the system shell before attempting to connect:
 """
 struct BlinkaBoard
     board
+    digitalio
     function BlinkaBoard()
-        new(pyimport("board"))
+        new(pyimport("board"),pyimport("digitalio"))        
     end
 end
 
@@ -30,6 +32,92 @@ Get the i2c channel of a `BlinkaBoard`
 """
 function i2c(b::BlinkaBoard)
     b.board.I2C()
+end
+
+"""
+```
+rawiopin(board,pin)
+```
+Get the python object representing the IO pin on a `BlinkaBoard`. This is currently specific
+to the MCP2221, we should split this into another package if we start using a bunch of GPIO
+boards
+"""
+rawiopin(b::BlinkaBoard,i::Int) = [b.board.G0,b.board.G1,b.board.G2,b.board.G3][i+1]
+
+"""
+```julia
+DigitalIOPin(board,pin_num)
+```
+Build an object representing a Digital IO pin on a `BlinkaBoard`. The pin will be
+configured as an output. This can be changed using `setinput!`.
+"""
+struct DigitalIOPin
+    board
+    pin
+    output #is this an output pin? false corresponds to input
+    function DigitalIOPin(b::BlinkaBoard,pin_num::Int)
+        pin = b.digitalio.DigitalInOut(rawiopin(b,pin_num))
+        pin.direction = b.digitalio.Direction.OUTPUT
+        new(b,pin,true)
+    end
+end
+
+"""
+```julia
+setinput!(pin)
+```
+Set a `DigitalIOPin` as an input
+"""
+function setinput!(pin::DigitalIOPin)
+    pin.pin.direction = pin.board.digitialio.Direction.INPUT
+end
+
+"""
+```julia
+setoutput!(pin)
+```
+Set a `DigitalIOPin` as an output
+"""
+function setoutput!(pin::DigitalIOPin)
+    pin.pin.direction = pin.board.digitialio.Direction.OUTPUT
+end
+
+"""
+```julia
+isoutput(pin)
+```
+Test if a `DigitalIOPin` is an output
+"""
+isoutput(pin::DigitalIOPin) = pin.output
+
+"""
+```julia
+isinput(pin)
+```
+Test if a `DigitalIOPin` is an input
+"""
+isinput(pin::DigitalIOPin) = !pin.output
+
+"""
+```julia
+digitalread(pin)
+```
+Read the value of a `DigitalIOPin`
+"""
+function digitalread(pin::DigitalIOPin)
+    @assert isinput(pin)
+    pin.pin.value
+end
+
+"""
+```julia
+digitalwrite!(pin,value)
+```
+Write a value to a `DigitalIOPin`
+"""
+function digitalwrite!(pin::DigitalIOPin,value::Bool)
+    @assert isoutput(pin)
+    pin.pin.value = value
 end
 
 end # module BlinkaBoards
